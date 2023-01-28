7aa6c29e62fa5a64941588aaf902db246a4c87de
Darioedx <dariovicentealmiron@gmail.com>
2023-01-28 12:02:56 +0100

    add iif and foreach

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,44 @@
-//jshint esversion:6
 
-let pokemonList = [
+let pokemonRepository = (function () {
+  
+  let pokemonList = [
     { name: 'Mr. Mime', height: 1.3, type: ['psychic' , 'fairy']},
     { name: 'Nidoran', height: 0.5, types: ['poison'] },
     { name: 'Golem', height: 1.4, types: ['rock', 'ground'] },
     { name: 'Exeggutor', height: 2, types: ['psychic', 'grass'] },
 ];
+  
+function add(pokemon) {
+    if (typeof pokemon === 'object' &&
+    'name' in pokemon )
+      if ('height' in pokemon && typeof pokemon.height === 'number') 
+        {
+          pokemonList.push(pokemon);
+        }
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+pokemonList = pokemonRepository.getAll(); 
+
+pokemonRepository.add({ name: 'Pikachu', height: 1.7, type: [] });
 
 
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height < 1.5)
-      {
-        document.write(`${pokemonList[i].name} (height:  ${pokemonList[i].height}) <br>`);
-      } 
-    else if (pokemonList[i].height > 1.5) 
-      {
-        document.write(`${pokemonList[i].name } (height: ${pokemonList[i].height}) - Wow! That's big!<br>`);
-      }
-}
+pokemonList.forEach(item =>{
+  if (item.height < 1.5)
+  {
+    document.write(`${item.name} (height:  ${item.height})<br>`);
+  } 
+  else if (item.height > 1.5) 
+  {
+    document.write(`${item.name } (height: ${item.height}) - Wow! That's big!<br>`);
+  }
+})
